0d2413ae08b3caab280d9bcbab6e925e6dc648e6
unknown <mobarakali62@gmail.com>
2020-11-04 00:16:25 +0600

    header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,45 +1,39 @@
 <template>
-    <div>
+    <nav>
         <v-layout tag="header" d-block class="white--text">
             <v-layout class="top-header-wrapper">
                 <v-container class="py-0">
-                    <v-row class="top-header mx-0">
-                        <v-col class="top-header-left pa-0">
+                    <v-layout class="top-header mx-0 wrap">
+                        <v-flex class="top-header-left flex-grow-0 d-flex align-center justify-space-between" >
                             <v-img
                                 width="171"
                                 height="29"
                                 contain
-                                :src="logo"
+                                class="flex-grow-0"
+                                :src="require('@/assets/images/logo.png')"
                             ></v-img>
-                        </v-col>
+                            <h5>sad</h5>
+                        </v-flex>
 
-                        <v-col class="top-header-mid pa-0">
-                            <v-col class="site-search pa-0">
-                                <v-autocomplete
-                                    v-model="values"
-                                    :items="items"
-                                    label="What are you looking for"
-                                    solo
-                                    append-icon="mdi-map-marker"
-                                >
-                                    <h1>hello</h1>
-                                </v-autocomplete>
-                            </v-col>
-                        </v-col>
-
-                        <v-col class="top-header-right pa-0">fsd</v-col>
-                    </v-row>
+                        <v-flex class="top-header-mid pa-0">
+                            <v-flex class="site-search pa-0">
+                                <div class="site-search-box">
+                                    <v-autocomplete height="44" full-width v-model="values" :items="items" label="What are you looking for" solo hide-details append-icon class="site-search-field"></v-autocomplete>
+                                </div>
+                                <button class="site-search-btn" type="submit"><i class="icofont-search"></i> Search</button>
+                            </v-flex>
+                        </v-flex>
+                        <v-flex class="top-header-right pa-0">fsd</v-flex>
+                    </v-layout>
                 </v-container>
             </v-layout>
             <v-layout class="btm-header-wrapper">sdf</v-layout>
         </v-layout>
 
         <v-card class="d-flex" flat>
-            <div>
-                I'm a flexbox container!
-            </div>
+            <div>I'm a flexbox container!</div>
         </v-card>
-    </div>
+    </nav>
 </template>
 
 <script>
@@ -47,9 +41,8 @@ export default {
     name: "SiteHeader",
 
     data: () => ({
-        logo: require("@/assets/images/logo.png"),
         items: ["foo", "bar", "fizz", "buzz"],
-        values: ["foo", "bar"],
+        values: [],
         value: null
     })
 };
