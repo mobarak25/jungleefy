c5fcdd6ad80a1eb9a26b19ed57db25463808691d
unknown <mobarakali62@gmail.com>
2020-11-05 00:43:01 +0600

    headr design

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,5 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style>
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -4,7 +4,9 @@
             <v-layout class="top-header-wrapper">
                 <v-container class="py-0">
                     <v-layout class="top-header mx-0 wrap">
-                        <v-flex class="top-header-left flex-grow-0 d-flex align-center justify-space-between" >
+                        <v-flex
+                            class="top-header-left flex-grow-0 d-flex align-center justify-space-between"
+                        >
                             <v-img
                                 width="171"
                                 height="29"
@@ -17,13 +19,73 @@
 
                         <v-flex class="top-header-mid pa-0">
                             <v-flex class="site-search pa-0">
-                                <div class="site-search-box">
-                                    <v-autocomplete height="44" full-width v-model="values" :items="items" label="What are you looking for" solo hide-details append-icon class="site-search-field"></v-autocomplete>
-                                </div>
-                                <button class="site-search-btn" type="submit"><i class="icofont-search"></i> Search</button>
+                                <v-flex class="site-search-box">
+                                    <v-autocomplete
+                                        height="44"
+                                        full-width
+                                        v-model="values"
+                                        :items="items"
+                                        label="What are you looking for"
+                                        solo
+                                        hide-details
+                                        append-icon
+                                        class="site-search-field"
+                                    ></v-autocomplete>
+                                </v-flex>
+                                <button class="site-search-btn" type="submit">
+                                    <i class="icofont-search"></i> Search
+                                </button>
                             </v-flex>
                         </v-flex>
-                        <v-flex class="top-header-right pa-0">fsd</v-flex>
+
+                        <!-- Top header right -->
+                        <v-layout justify-end class="top-header-right">
+                            <v-flex class="top-header-widgets flex-grow-0">
+                                <a href="#">
+                                    <v-flex class="header-widgets-icon">
+                                        <i class="icofont-heart-alt"></i>
+                                        <v-flex class="count">15</v-flex>
+                                    </v-flex>
+                                    <span>Watchlist</span>
+                                </a>
+                            </v-flex>
+
+                            <v-flex class="top-header-widgets flex-grow-0">
+                                <a href="#">
+                                    <v-flex class="header-widgets-icon">
+                                        <i class="icofont-shopping-cart"></i>
+                                        <v-flex class="count">5</v-flex>
+                                    </v-flex>
+                                    <span>My Cart</span>
+                                </a>
+                            </v-flex>
+
+                            <v-flex class="top-header-widgets flex-grow-0">
+                                <v-menu offset-y transition="slide-x-transition" :offset-x="offset">
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <!-- <a v-bind="attrs" v-on="on">Dropdown</a> -->
+                                        <v-flex v-bind="attrs" v-on="on" class="pointer">
+                                            <v-flex class="header-widgets-icon">
+                                                <i class="icofont-ui-user"></i>
+                                                <i class="icofont-simple-down down-arrow"></i>
+                                            </v-flex>
+                                            <span
+                                                class="text-truncate sign-text"
+                                            >Hey Sign In Mobarak</span>
+                                        </v-flex>
+                                    </template>
+                                    <v-list>
+                                        <v-list-item
+                                            v-for="(item, index) in userInfos"
+                                            :key="index"
+                                        >
+                                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                        </v-list-item>
+                                    </v-list>
+                                </v-menu>
+                            </v-flex>
+                        </v-layout>
+                        <!-- //Top header right -->
                     </v-layout>
                 </v-container>
             </v-layout>
@@ -33,6 +95,17 @@
         <v-card class="d-flex" flat>
             <div>I'm a flexbox container!</div>
         </v-card>
+
+        <v-menu offset-y max-width="200">
+            <template v-slot:activator="{ on, attrs }">
+                <a v-bind="attrs" v-on="on">Dropdown</a>
+            </template>
+            <v-list>
+                <v-list-item v-for="(item, index) in userInfos" :key="index">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
     </nav>
 </template>
 
@@ -43,7 +116,15 @@ export default {
     data: () => ({
         items: ["foo", "bar", "fizz", "buzz"],
         values: [],
-        value: null
+        value: null,
+        offset: true,
+
+        userInfos: [
+            { title: "Click Me Click Me Click Me" },
+            { title: "Click Me" },
+            { title: "Click Me" },
+            { title: "Click Me 2" }
+        ]
     })
 };
 </script>
