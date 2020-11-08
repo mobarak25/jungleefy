aa30226eff3fa5a59caec82a9e8ac64bff0738be
unknown <mobarakali62@gmail.com>
2020-11-08 17:05:04 +0600

    bannder design

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,9 +1,23 @@
 <template>
-    <v-layout tag="section">
-        <v-img :src="require('@/assets/images/home_banner.png')">
-            <v-flex>fdsfdgsdfgsdfgsdfgsdfgdsfg gasgdfgdfg gfsdfgdsfg</v-flex>
-        </v-img>
-    </v-layout>
+    <div>
+        <v-layout tag="section" class="banner-wrapper">
+            <v-img :src="require('@/assets/images/home_banner.png')">
+                <v-flex class="banner-caption d-flex fill-height align-center">
+                    <v-container>
+                        <v-btn href="#" tile depressed class="brand white--text">
+                            START FROM
+                            <span>TK 200</span>
+                        </v-btn>
+                    </v-container>
+                </v-flex>
+            </v-img>
+        </v-layout>
+        <v-row no-gutters>
+            <v-col sm="6">SDFSFD</v-col>
+            <v-col sm="6">SDFSFD</v-col>
+            <v-col sm="6">SDFSFD</v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
@@ -15,3 +29,6 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+</style>
