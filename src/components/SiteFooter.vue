7ef58bd302a2101cf4e80a38a237c20554f53672
unknown <mobarakali62@gmail.com>
2020-11-11 22:28:47 +0600

    footer design

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <v-footer color="secondary" dark padless class="d-block site-footer">
-        <v-layout>
+        <v-layout class="footer-top">
             <v-container>
                 <v-row no-gutters>
                     <v-col class="footer-box">
@@ -12,17 +12,67 @@
                             :src="require('@/assets/images/logo.png')"
                             alt="Footer Logo"
                         ></v-img>
+
                         <h3>Get in Tuch</h3>
-                        <!-- <div class="text-center">
-                            <v-avatar tile color="blue">
-                                <v-icon dark>mdi-alarm</v-icon>
-                            </v-avatar>
-                        </div>-->
+                        <v-flex class="pt-5 footer-social">
+                            <v-btn
+                                height="48"
+                                width="48"
+                                fab
+                                outlined
+                                text
+                                tile
+                                v-for="icon in icons"
+                                :key="icon"
+                            >
+                                <v-icon>{{icon}}</v-icon>
+                            </v-btn>
+                        </v-flex>
                     </v-col>
-                    <v-col></v-col>
-                    <v-col>sdf</v-col>
-                    <v-col>sdf</v-col>
-                    <v-col>sdf</v-col>
+
+                    <v-col class="footer-box">
+                        <h3>How it Works</h3>
+                        <v-flex>
+                            <ul>
+                                <li v-for="(work, index) in howWorks" :key="index">
+                                    <a href="#">{{work.text}}</a>
+                                </li>
+                            </ul>
+                        </v-flex>
+                    </v-col>
+
+                    <v-col class="footer-box">
+                        <h3>Categories</h3>
+                        <v-flex>
+                            <ul>
+                                <li v-for="(category, index) in categories" :key="index">
+                                    <a href="#">{{category.text}}</a>
+                                </li>
+                            </ul>
+                        </v-flex>
+                    </v-col>
+
+                    <v-col class="footer-box">
+                        <h3>My Account</h3>
+                        <v-flex>
+                            <ul>
+                                <li v-for="(accountInfo, index) in accountInfos" :key="index">
+                                    <a href="#">{{accountInfo.text}}</a>
+                                </li>
+                            </ul>
+                        </v-flex>
+                    </v-col>
+
+                    <v-col class="footer-box">
+                        <h3>Information</h3>
+                        <v-flex>
+                            <ul>
+                                <li v-for="(information, index) in informations" :key="index">
+                                    <a href="#">{{information.text}}</a>
+                                </li>
+                            </ul>
+                        </v-flex>
+                    </v-col>
                 </v-row>
             </v-container>
         </v-layout>
@@ -43,12 +93,40 @@
 <script>
 export default {
     data: () => ({
-        icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
-        selectedItem: 1,
-        items: [
-            { text: "Real-Time", icon: "mdi-clock" },
-            { text: "Audience", icon: "mdi-account" },
-            { text: "Conversions", icon: "mdi-flag" }
+        icons: ["mdi-twitter", "mdi-facebook", "mdi-youtube", "mdi-linkedin"],
+
+        howWorks: [
+            { id: 1, text: "Online Auction" },
+            { id: 2, text: "How to Bid" },
+            { id: 3, text: "Auction Fees" },
+            { id: 4, text: "Shipping Fees" },
+            { id: 5, text: "Auction FAQ's" }
+        ],
+        categories: [
+            { id: 1, text: "Drop shipping" },
+            { id: 2, text: "Bulk selling" },
+            { id: 3, text: "Warehouse Products" },
+            { id: 4, text: "Warehouse Closeouts" },
+            { id: 5, text: "Overstock Surplus" },
+            { id: 6, text: "Wholesale Stock" },
+            { id: 7, text: "Manufacturer Stock" }
+        ],
+        accountInfos: [
+            { id: 1, text: "My Account" },
+            { id: 2, text: "Orders" },
+            { id: 3, text: "Addresses" },
+            { id: 4, text: "Cancellation, Return & Refund Policy" },
+            { id: 5, text: "Manage Your Content & Devices" },
+            { id: 6, text: "Help" }
+        ],
+        informations: [
+            { id: 1, text: "About Us" },
+            { id: 2, text: "Contact" },
+            { id: 3, text: "Our Blog" },
+            { id: 4, text: "Careers" },
+            { id: 5, text: "Delivery Information" },
+            { id: 6, text: "Privacy Policy" },
+            { id: 7, text: "Terms & Conditions" }
         ]
     })
 };
