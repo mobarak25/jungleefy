db3863f009f3d199003193cf5132a52934b3c04f
unknown <mobarakali62@gmail.com>
2020-12-22 18:29:22 +0600

    post a add

--- src/views/createAFreeAd.vue
+++ src/views/createAFreeAd.vue
@@ -28,27 +28,278 @@
 
                         <ValidationObserver v-slot="{ handleSubmit }">
                             <form @submit.prevent="handleSubmit(onSubmit)">
+                                <!-- Condition section-->
                                 <ValidationProvider
-                                    name="E-mail"
-                                    rules="required|email"
-                                    v-slot="{ errors }"
+                                    tag="div"
+                                    class="conditions d-flex align-center"
+                                    name="Condition"
+                                    rules="required"
+                                    :bails="false"
+                                    v-slot="{ errors, classes }"
                                 >
-                                    <input
-                                        v-model="formDara.email"
-                                        type="email"
-                                        placeholder="email"
-                                    />
-                                    <span>{{ errors[0] }}</span>
+                                    <h4>Condition</h4>
+                                    <div class="control" :class="classes">
+                                        <v-radio-group
+                                            v-model="formData.condition"
+                                            row
+                                            hide-details
+                                        >
+                                            <v-radio color="brand" label="New" value="New"></v-radio>
+                                            <v-radio color="brand" label="Used" value="Used"></v-radio>
+                                        </v-radio-group>
+                                        <span>{{ errors[0] }}</span>
+                                    </div>
                                 </ValidationProvider>
 
-                                <ValidationProvider
-                                    name="Name"
-                                    rules="required|alpha"
-                                    v-slot="{ errors }"
+                                <v-row>
+                                    <ValidationProvider
+                                        tag="div"
+                                        class="col-md-6 py-0"
+                                        name="Ad Title"
+                                        rules="required|alpha"
+                                        :bails="false"
+                                        v-slot="{ errors, classes }"
+                                    >
+                                        <div class="input-wrap" :class="classes">
+                                            <v-flex class="input-label">Ad Title*</v-flex>
+                                            <v-text-field
+                                                solo
+                                                flat
+                                                hide-details
+                                                outlined
+                                                placeholder="Type your ad Title*"
+                                                v-model="formData.ad_title"
+                                            ></v-text-field>
+                                            <span>{{ errors[0] }}</span>
+                                        </div>
+                                    </ValidationProvider>
+
+                                    <ValidationProvider
+                                        tag="div"
+                                        class="col-md-6 py-0"
+                                        name="Brand"
+                                        rules="required"
+                                        :bails="false"
+                                        v-slot="{ errors, classes }"
+                                    >
+                                        <div class="input-wrap" :class="classes">
+                                            <v-flex class="input-label">Brand</v-flex>
+
+                                            <v-select
+                                                :items="brands"
+                                                label="Solo field"
+                                                solo
+                                                flat
+                                                hide-details
+                                                outlined
+                                                v-model="formData.brand"
+                                            ></v-select>
+                                            <span>{{ errors[0] }}</span>
+                                        </div>
+                                    </ValidationProvider>
+                                </v-row>
+
+                                <v-row>
+                                    <ValidationProvider
+                                        tag="div"
+                                        class="col-md-6"
+                                        name="Product Type"
+                                        rules="required"
+                                        :bails="false"
+                                        v-slot="{ errors, classes }"
+                                    >
+                                        <div class="control" :class="classes">
+                                            <v-flex class="input-label">Product Type</v-flex>
+                                            <v-select
+                                                :items="product_types"
+                                                label="Solo field"
+                                                solo
+                                                flat
+                                                hide-details
+                                                outlined
+                                                v-model="formData.product_type"
+                                            ></v-select>
+                                            <span>{{ errors[0] }}</span>
+                                        </div>
+                                    </ValidationProvider>
+
+                                    <ValidationProvider
+                                        tag="div"
+                                        class="col-md-6"
+                                        name="Product Quantity"
+                                        rules="required"
+                                        :bails="false"
+                                        v-slot="{ errors, classes }"
+                                    >
+                                        <div class="control" :class="classes">
+                                            <v-flex class="input-label">Product Quantity</v-flex>
+                                            <v-text-field
+                                                solo
+                                                flat
+                                                hide-details
+                                                outlined
+                                                type="number"
+                                                placeholder="Type Your Product Quantity"
+                                                v-model="formData.quantity"
+                                            ></v-text-field>
+                                            <span>{{ errors[0] }}</span>
+                                        </div>
+                                    </ValidationProvider>
+                                </v-row>
+
+                                <!-- check auction -->
+                                <v-radio-group v-model="formData.auction" row hide-details>
+                                    <v-row class="auction-check">
+                                        <v-col md="6">
+                                            <v-radio
+                                                color="brand"
+                                                label="Sale with Auction"
+                                                value="with_auction"
+                                            ></v-radio>
+                                        </v-col>
+                                        <v-col md="6">
+                                            <v-radio
+                                                color="brand"
+                                                label="Sale without  Auction"
+                                                value="without_auction"
+                                            ></v-radio>
+                                        </v-col>
+                                    </v-row>
+                                </v-radio-group>
+
+                                <v-flex
+                                    v-if="formData.auction=='with_auction'"
+                                    class="with-auction"
                                 >
-                                    <input v-model="formDara.name" type="text" placeholder="Name" />
-                                    <span>{{ errors[0] }}</span>
-                                </ValidationProvider>
+                                    <v-row>
+                                        <ValidationProvider
+                                            tag="div"
+                                            class="col-md-6"
+                                            name="Starting Price"
+                                            rules="required"
+                                            :bails="false"
+                                            v-slot="{ errors, classes }"
+                                        >
+                                            <div class="control d-flex flex-wrap" :class="classes">
+                                                <v-flex
+                                                    class="input-label flex-grow-0"
+                                                >Starting Price (TK)</v-flex>
+                                                <v-flex>
+                                                    <v-text-field
+                                                        solo
+                                                        flat
+                                                        hide-details
+                                                        outlined
+                                                        type="number"
+                                                        placeholder="Example 8000 TK"
+                                                        v-model="formData.starting_price"
+                                                    ></v-text-field>
+                                                    <span>{{ errors[0] }}</span>
+                                                </v-flex>
+                                                <v-tooltip right>
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-icon
+                                                            v-bind="attrs"
+                                                            v-on="on"
+                                                        >mdi-information-outline</v-icon>
+                                                    </template>
+                                                    <span class="tooltip-info">
+                                                        Lorem ipsum, dolor sit
+                                                        <br />amet consectetur
+                                                        <br />adipisicing elit.
+                                                        <br />Dolorem facilis unde
+                                                        <br />fugiat eum iure sequi
+                                                    </span>
+                                                </v-tooltip>
+                                            </div>
+                                        </ValidationProvider>
+
+                                        <ValidationProvider
+                                            tag="div"
+                                            class="col-md-6"
+                                            name="Buy Now Price"
+                                            rules="required"
+                                            :bails="false"
+                                            v-slot="{ errors, classes }"
+                                        >
+                                            <div class="control d-flex flex-wrap" :class="classes">
+                                                <v-flex
+                                                    class="input-label flex-grow-0"
+                                                >Buy Now Price</v-flex>
+                                                <v-flex>
+                                                    <v-text-field
+                                                        solo
+                                                        flat
+                                                        hide-details
+                                                        outlined
+                                                        type="number"
+                                                        placeholder="Example 8000 TK"
+                                                        v-model="formData.buy_price"
+                                                    ></v-text-field>
+                                                    <span>{{ errors[0] }}</span>
+                                                </v-flex>
+                                                <v-tooltip right>
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-icon
+                                                            v-bind="attrs"
+                                                            v-on="on"
+                                                        >mdi-information-outline</v-icon>
+                                                    </template>
+                                                    <span class="tooltip-info">
+                                                        Lorem ipsum, dolor sit
+                                                        <br />amet consectetur
+                                                        <br />adipisicing elit.
+                                                        <br />Dolorem facilis unde
+                                                        <br />fugiat eum iure sequi
+                                                    </span>
+                                                </v-tooltip>
+                                            </div>
+                                        </ValidationProvider>
+
+                                        <ValidationProvider
+                                            tag="div"
+                                            class="col-md-12"
+                                            name="Approximately Price"
+                                            rules="required"
+                                            :bails="false"
+                                            v-slot="{ errors, classes }"
+                                        >
+                                            <div class="control d-flex flex-wrap" :class="classes">
+                                                <v-flex
+                                                    class="input-label flex-grow-0"
+                                                >Approximately Price (TK)</v-flex>
+                                                <v-flex>
+                                                    <v-text-field
+                                                        solo
+                                                        flat
+                                                        hide-details
+                                                        outlined
+                                                        type="number"
+                                                        placeholder="Example 8000 TK"
+                                                        v-model="formData.approximately_price"
+                                                    ></v-text-field>
+                                                    <span>{{ errors[0] }}</span>
+                                                </v-flex>
+                                                <v-tooltip right>
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-icon
+                                                            v-bind="attrs"
+                                                            v-on="on"
+                                                        >mdi-information-outline</v-icon>
+                                                    </template>
+                                                    <span class="tooltip-info">
+                                                        Lorem ipsum, dolor sit
+                                                        <br />amet consectetur
+                                                        <br />adipisicing elit.
+                                                        <br />Dolorem facilis unde
+                                                        <br />fugiat eum iure sequi
+                                                    </span>
+                                                </v-tooltip>
+                                            </div>
+                                        </ValidationProvider>
+                                    </v-row>
+                                </v-flex>
+
                                 <v-btn type="submit">Submit</v-btn>
                             </form>
                         </ValidationObserver>
@@ -69,9 +320,16 @@ export default {
 
     data() {
         return {
-            formDara: {
-                name: "",
-                email: "",
+            formData: {
+                condition: "Used",
+                ad_title: "",
+                brand: "",
+                product_type: "",
+                quantity: "",
+                auction: "with_auction",
+                starting_price: "",
+                buy_price: "",
+                approximately_price: "",
             },
             selectedCategories: [],
             breadcrumbItems: [
@@ -91,6 +349,8 @@ export default {
                     href: "breadcrumbs_link_1",
                 },
             ],
+            brands: ["Hp", "Samsung", "LG", "Sony", "Vizio", "TCL"],
+            product_types: ["Hp", "Samsung", "LG", "Sony", "Vizio", "TCL"],
         };
     },
     created() {
